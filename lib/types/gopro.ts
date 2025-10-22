22c0e647b7d3284e08563235d2e451b7b2144b95
Ilias Haddad <iliasshaddad18@gmail.com>
2025-10-22 10:25:33 +0100

    feat: add gopro utility function to get metadata

---
+++ lib/types/gopro.ts
@@ -0,0 +1,4 @@
+export interface GoProMetadata {
+  [key: string]: any;
+  'device name'?: string;
+}
---
+++ lib/utils/gopro.ts
@@ -0,0 +1,42 @@
+import goproTelemetry from 'gopro-telemetry'
+import gpmfExtract from 'gpmf-extract'
+import { readFileSync } from 'node:fs'
+import { GoProMetadata } from '../types/gopro'
+
+export async function getGoProVideoMetadata(videoFullPath: string) {
+  return new Promise((res, rej) => {
+    const file = readFileSync(videoFullPath)
+    gpmfExtract(file)
+      .then((extracted) => {
+        goproTelemetry(extracted, {}, (telemetry) => {
+          res(telemetry)
+        })
+      })
+      .catch(rej)
+  })
+}
+export function getGoProDeviceName(metadata: GoProMetadata): string {
+  for (const key in metadata) {
+    if (metadata[key]?.['device name']) {
+      return metadata[key]['device name']
+    }
+  }
+  return 'Unknown GoPro Device'
+}
+
+export function extractGPS(metadata: GoProMetadata): { lat: number; lon: number; alt?: number }[] {
+  const gpsData: { lat: number; lon: number; alt?: number }[] = []
+
+  if (metadata.streams?.GPS5) {
+    const gps5 = metadata.streams.GPS5
+
+    gps5.samples.slice(0, 5).forEach((sample: any) => {
+      if (Array.isArray(sample.value)) {
+        const [lat, lon, alt] = sample.value
+        gpsData.push({ lat, lon, alt })
+      }
+    })
+  }
+
+  return gpsData
+}
