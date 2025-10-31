6900fbd06e47a0255104f862d473969ea7ccbb3f
Ilias Haddad <iliasshaddad18@gmail.com>
2025-10-31 21:22:10 +0100

    feat: add vitest config file

---
+++ vitest.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vitest/config'
+import path from 'path'
+
+export default defineConfig({
+  test: {
+    globals: true,
+    logHeapUsage: true,
+    environment: 'node',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: ['node_modules/', 'tests/', '**/*.spec.ts', '**/*.test.ts'],
+    },
+    testTimeout: 30000 * 2,
+    hookTimeout: 30000 * 2,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname),
+      '@/app': path.resolve(__dirname, './app'),
+      '@/lib': path.resolve(__dirname, './lib'),
+      '@/resources': path.resolve(__dirname, './resources'),
+    },
+  },
+})
