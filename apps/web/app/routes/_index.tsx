e5dd1b4452be4b7580a6bfe23196c40e30006eb7
Ilias Haddad <iliasshaddad18@gmail.com>
2025-11-13 17:47:28 +0100

    feat: add index page to handle redirect between auth and onboarding

---
+++ apps/web/app/routes/_index.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from 'react'
+import { redirect, useLoaderData, useNavigate, type LoaderFunctionArgs } from 'react-router'
+import { useOnboarding } from '~/hooks/useOnboarding'
+import { getUser } from '~/services/user.sever'
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const user = await getUser(request)
+
+  if (!user) {
+    return { user: null }
+  }
+
+  return redirect('/app/home')
+}
+
+export default function IndexPage() {
+  const { user } = useLoaderData<typeof loader>()
+  const navigate = useNavigate()
+  const { isOnboardingComplete } = useOnboarding()
+
+  useEffect(() => {
+    if (!user) {
+      if (isOnboardingComplete) {
+        navigate('/auth/login')
+      } else {
+        navigate('/onboarding')
+      }
+    }
+  }, [user, navigate, isOnboardingComplete])
+
+  return null
+}
