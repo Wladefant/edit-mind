774a550b026a2428196ef5203552b07b42699d61
Ilias Haddad <iliasshaddad18@gmail.com>
2025-11-13 17:36:13 +0100

    remove unused pages for the web UI

--- apps/web/app/routes/auth.forgot-password.tsx
+++
@@ -1,8 +0,0 @@
-import type { MetaFunction } from "react-router";
-
-export const meta: MetaFunction = () => {
-  return [{ title: 'Reset your password | Edit Mind' }]
-}
-export default function ResetPassword() {
-  return <div>Reset Password Page</div>;
-}
