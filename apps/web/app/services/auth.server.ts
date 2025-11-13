b0388872514deb14f787180031bb037f3774cf60
Ilias Haddad <iliasshaddad18@gmail.com>
2025-11-13 17:37:14 +0100

    fix: update auth services to handle only login for now

--- apps/web/app/services/auth.server.ts
+++ apps/web/app/services/auth.server.ts
@@ -1,73 +1,34 @@
 import { z } from 'zod'
-import { RegisterSchema, LoginSchema } from '~/schemas/auth'
+import { LoginSchema } from '~/schemas/auth'
 import { prisma } from './database'
 import { getSession, commitSession } from './session'
 import bcrypt from 'bcryptjs'
-
-export async function register(request: Request, values: z.infer<typeof RegisterSchema>) {
-  const user = await prisma.user.findUnique({
-    where: {
-      email: values.email
-    }
-  })
-
-  if (user) {
-    return {
-      error: 'User already exists'
-    }
-  }
-
-  const passwordHash = await bcrypt.hash(values.password, 10)
-
-  const newUser = await prisma.user.create({
-    data: {
-      name: values.fullName,
-      email: values.email,
-      password: passwordHash
-    }
-  })
-
-  const session = await getSession(request.headers.get('Cookie'))
-  session.set('userId', newUser.id)
-
-  return new Response(null, {
-    status: 302,
-    headers: {
-      Location: '/app',
-      'Set-Cookie': await commitSession(session)
-    }
-  })
-}
+import { redirect } from 'react-router'
 
 export async function login(request: Request, values: z.infer<typeof LoginSchema>) {
   const user = await prisma.user.findUnique({
     where: {
-      email: values.email
-    }
+      email: values.email,
+    },
   })
 
   if (!user) {
-    return {
-      error: 'Invalid email or password'
-    }
+    return new Response(JSON.stringify({ error: 'Invalid email or password' }), { status: 400 })
   }
 
   const passwordMatch = await bcrypt.compare(values.password, user.password)
 
   if (!passwordMatch) {
-    return {
-      error: 'Invalid email or password'
-    }
+    return new Response(JSON.stringify({ error: 'Invalid email or password' }), { status: 400 })
   }
 
   const session = await getSession(request.headers.get('Cookie'))
   session.set('userId', user.id)
 
-  return new Response(null, {
-    status: 302,
+  return redirect(JSON.stringify({ session: { email: user.email, name: user.name } }), {
+    status: 201,
     headers: {
-      Location: '/app',
-      'Set-Cookie': await commitSession(session)
-    }
+      'Set-Cookie': await commitSession(session),
+    },
   })
 }
