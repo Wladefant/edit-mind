e8a15c983908cf1cb2d2f3653f29ebce3895eca3
Ilias Haddad <iliasshaddad18@gmail.com>
2025-11-20 10:40:16 +0100

    fix: update face types with job queue types

--- packages/shared/types/face.ts
+++ packages/shared/types/face.ts
@@ -1,8 +1,7 @@
-import z from "zod";
-import { unknownFace } from "../schemas";
+import z from 'zod'
+import { unknownFace } from '../schemas'
 
 export type UnknownFace = z.infer<typeof unknownFace>
-
 
 export interface FaceIndexingProgress {
   progress: number
@@ -11,4 +10,32 @@ export interface FaceIndexingProgress {
 export interface KnownFace {
   name: string
   images: string[]
+}
+export interface AddFaceLabelingJobParams {
+  personName: string
+  referenceImages: string[]
+  unknownFacesDir: string
+}
+
+export type FaceIndexProgress = {
+  progress: number
+  elapsed: string
+}
+
+export interface FaceLabelingJobData {
+  personName: string
+  referenceImages: string[]
+  unknownFacesDir: string
+}
+
+export interface MatchResult {
+  json_file: string
+  image_file: string
+  confidence: number
+  face_id: string
+  face_data: {
+    video_path: string
+    frame_start_time_ms: number
+    frame_end_time_ms: number
+  }
 }
