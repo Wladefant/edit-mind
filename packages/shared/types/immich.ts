7c27f7a2d98681cfd5a396d04e73e109d5cca24f
Ilias Haddad <iliasshaddad18@gmail.com>
2025-11-21 14:44:20 +0100

    fix: update immich types

--- packages/shared/types/immich.ts
+++ packages/shared/types/immich.ts
@@ -1,3 +1,38 @@
+import z from 'zod'
+import { immichConfigFormSchema } from '../schemas/immich'
+
 export interface ImmichImporterJobData {
-  apiKey: string
+  integrationId: string
+}
+
+export type ImmichConfig = z.infer<typeof immichConfigFormSchema>
+
+export interface Person {
+  id: string
+  name: string
+}
+
+export interface PeopleResponse {
+  people: Person[]
+  hasNextPage: boolean
+}
+
+export interface TimeBucket {
+  timeBucket: string
+  count: number
+}
+
+export interface AssetsBucketResponse {
+  id: string[]
+}
+
+export interface Face {
+  id: string
+  person: {
+    id: string
+  }
+  boundingBoxX1: number
+  boundingBoxY1: number
+  boundingBoxX2: number
+  boundingBoxY2: number
 }
